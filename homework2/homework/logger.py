591a66fb78a154b28094d283377f94d596260f67
Zach Li <120526833+zachli88@users.noreply.github.com>
2025-09-29 15:36:06 -0500

    Add files via upload

--- homework2/homework/logger.py
+++ homework2/homework/logger.py
@@ -1,1 +1,59 @@
+from datetime import datetime
+from pathlib import Path
 
+import torch
+import torch.utils.tensorboard as tb
+
+
+def test_logging(logger: tb.SummaryWriter):
+    """
+    Your code here - finish logging the dummy loss and accuracy
+
+    For training, log the training loss every iteration and the average accuracy every epoch
+    Call the loss 'train_loss' and accuracy 'train_accuracy'
+
+    For validation, log only the average accuracy every epoch
+    Call the accuracy 'val_accuracy'
+
+    Make sure the logging is in the correct spot so the global_step is set correctly,
+    for epoch=0, iteration=0: global_step=0
+    """
+    # strongly simplified training loop
+    global_step = 0
+    for epoch in range(10):
+        metrics = {"train_acc": [], "val_acc": []}
+
+        # example training loop
+        torch.manual_seed(epoch)
+        for iteration in range(20):
+            dummy_train_loss = 0.9 ** (epoch + iteration / 20.0)
+            dummy_train_accuracy = epoch / 10.0 + torch.randn(10)
+
+            # TODO: log train_loss
+            # TODO: save additional metrics to be averaged
+
+            global_step += 1
+
+        # TODO: log average train_accuracy
+
+        # example validation loop
+        torch.manual_seed(epoch)
+        for _ in range(10):
+            dummy_validation_accuracy = epoch / 10.0 + torch.randn(10)
+
+            # TODO: save additional metrics to be averaged
+
+        # TODO: log average val_accuracy
+
+
+if __name__ == "__main__":
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+    parser.add_argument("--exp_dir", type=str, default="logs")
+    args = parser.parse_args()
+
+    log_dir = Path(args.exp_dir) / f"logger_{datetime.now().strftime('%m%d_%H%M%S')}"
+    logger = tb.SummaryWriter(log_dir)
+
+    test_logging(logger)
---
+++ homework2/homework/models.py
@@ -0,0 +1,172 @@
+"""
+Implement the following models for classification.
+
+Feel free to modify the arguments for each of model's __init__ function.
+This will be useful for tuning model hyperparameters such as hidden_dim, num_layers, etc,
+but remember that the grader will assume the default constructor!
+"""
+
+from pathlib import Path
+
+import torch
+import torch.nn as nn
+
+
+class ClassificationLoss(nn.Module):
+    def forward(self, logits: torch.Tensor, target: torch.LongTensor) -> torch.Tensor:
+        """
+        Multi-class classification loss
+        Hint: simple one-liner
+
+        Args:
+            logits: tensor (b, c) logits, where c is the number of classes
+            target: tensor (b,) labels
+
+        Returns:
+            tensor, scalar loss
+        """
+        raise NotImplementedError("ClassificationLoss.forward() is not implemented")
+
+
+class LinearClassifier(nn.Module):
+    def __init__(
+        self,
+        h: int = 64,
+        w: int = 64,
+        num_classes: int = 6,
+    ):
+        """
+        Args:
+            h: int, height of the input image
+            w: int, width of the input image
+            num_classes: int, number of classes
+        """
+        super().__init__()
+
+        raise NotImplementedError("LinearClassifier.__init__() is not implemented")
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Args:
+            x: tensor (b, 3, H, W) image
+
+        Returns:
+            tensor (b, num_classes) logits
+        """
+        raise NotImplementedError("LinearClassifier.forward() is not implemented")
+
+
+class MLPClassifier(nn.Module):
+    def __init__(
+        self,
+        h: int = 64,
+        w: int = 64,
+        num_classes: int = 6,
+    ):
+        """
+        An MLP with a single hidden layer
+
+        Args:
+            h: int, height of the input image
+            w: int, width of the input image
+            num_classes: int, number of classes
+        """
+        super().__init__()
+
+        raise NotImplementedError("MLPClassifier.__init__() is not implemented")
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Args:
+            x: tensor (b, 3, H, W) image
+
+        Returns:
+            tensor (b, num_classes) logits
+        """
+        raise NotImplementedError("MLPClassifier.forward() is not implemented")
+
+
+class MLPClassifierDeep(nn.Module):
+    def __init__(
+        self,
+        h: int = 64,
+        w: int = 64,
+        num_classes: int = 6,
+    ):
+        """
+        An MLP with multiple hidden layers
+
+        Args:
+            h: int, height of image
+            w: int, width of image
+            num_classes: int
+
+        Hint - you can add more arguments to the constructor such as:
+            hidden_dim: int, size of hidden layers
+            num_layers: int, number of hidden layers
+        """
+        super().__init__()
+
+        raise NotImplementedError("MLPClassifierDeep.__init__() is not implemented")
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Args:
+            x: tensor (b, 3, H, W) image
+
+        Returns:
+            tensor (b, num_classes) logits
+        """
+        raise NotImplementedError("MLPClassifierDeep.forward() is not implemented")
+
+
+model_factory = {
+    "linear": LinearClassifier,
+    "mlp": MLPClassifier,
+    "mlp_deep": MLPClassifierDeep,
+}
+
+
+def calculate_model_size_mb(model: torch.nn.Module) -> float:
+    """
+    Args:
+        model: torch.nn.Module
+
+    Returns:
+        float, size in megabytes
+    """
+    return sum(p.numel() for p in model.parameters()) * 4 / 1024 / 1024
+
+
+def save_model(model):
+    """
+    Use this function to save your model in train.py
+    """
+    for n, m in model_factory.items():
+        if isinstance(model, m):
+            return torch.save(model.state_dict(), Path(__file__).resolve().parent / f"{n}.th")
+    raise ValueError(f"Model type '{str(type(model))}' not supported")
+
+
+def load_model(model_name: str, with_weights: bool = False, **model_kwargs):
+    """
+    Called by the grader to load a pre-trained model by name
+    """
+    r = model_factory[model_name](**model_kwargs)
+    if with_weights:
+        model_path = Path(__file__).resolve().parent / f"{model_name}.th"
+        assert model_path.exists(), f"{model_path.name} not found"
+        try:
+            r.load_state_dict(torch.load(model_path, map_location="cpu"))
+        except RuntimeError as e:
+            raise AssertionError(
+                f"Failed to load {model_path.name}, make sure the default model arguments are set correctly"
+            ) from e
+
+    # Limit model sizes since they will be zipped and submitted
+    model_size_mb = calculate_model_size_mb(r)
+    if model_size_mb > 10:
+        raise AssertionError(f"{model_name} is too large: {model_size_mb:.2f} MB")
+    print(f"Model size: {model_size_mb:.2f} MB")
+
+    return r
