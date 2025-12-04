0d30b966b1f926e1b1d2c18a127976469b77b636
root <lizach739@gmail.com>
2025-12-04 04:48:25 +0000

    base

--- homework6/homework/base_llm.py
+++ homework6/homework/base_llm.py
@@ -22,13 +22,14 @@ class BaseLLM:
 
     def format_prompt(self, question: str) -> str:
         messages = [
-            {"role": "system", "content": "You are an ai assistant. Be short and to the point."},
+            {"role": "system", "content": "You are a helpful assistant. Be concise."},
             {"role": "user", "content": question},
         ]
 
         return self.tokenizer.apply_chat_template(
             messages, add_generation_prompt=True, tokenize=False
         )
+
 
 
     def parse_answer(self, answer: str) -> float:
